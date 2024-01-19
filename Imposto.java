c8a7d6546a55b010dfadf6ea668e3404130dc904
Rafael Mendes <rafaelmendesctt@gmail.com>
2024-01-19 16:30:33 -0300

    Classe Main renomeada para Imposto

--- Imposto.java
+++ Imposto.java
@@ -3,7 +3,7 @@ package DevSuperiorDesafioImposto;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class Imposto {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
